7af1f534bfc0971057893fd4b7e48847ee1f92df
ryans <ryans@jlab.org>
2025-08-25 11:25:46 -0400

    Generated docs

---
+++ src/main/java/org/jlab/phaser/swing/generated/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the classes generated with Netbeans Matisse builder tool.   Classes in this package should be edited
+ * very carefully and flagged code blocks should be edited with the builder tool.  Observe the *.form files.
+ */
+package org.jlab.phaser.swing.generated;
