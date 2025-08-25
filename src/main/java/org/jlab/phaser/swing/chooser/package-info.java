03d245794d3664ff4217d74a031659d66b679ae9
ryans <ryans@jlab.org>
2025-08-25 13:58:14 -0400

    format

--- src/main/java/org/jlab/phaser/swing/chooser/package-info.java
+++ src/main/java/org/jlab/phaser/swing/chooser/package-info.java
@@ -1,4 +1,2 @@
-/**
- * Provides the Phaser Swing Client Swing chooser dialog non-generated classes.
- */
+/** Provides the Phaser Swing Client Swing chooser dialog non-generated classes. */
 package org.jlab.phaser.swing.chooser;
